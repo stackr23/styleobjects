7ddf654e3dc25285aa207d22e05332d4de859927
DoubleU23 <dev@project23.org>
2018-11-07 21:40:29 +0100

    :white_check_mark: added tests for .convert()"

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,9 +1,33 @@
-// let assert    = require('assert'),
-//     nativeCSS = require('../bin/native-css/src/native-css.js'),
-//     lib       = require('../bin/native-css/lib/index.js')
+let assert    = require('assert'),
+    expect    = require('chai').expect,
+    nativeCSS = require('../bin/native-css/src/native-css.js'),
+    lib       = require('../bin/native-css/lib/index.js')
 
-// describe('Parser', function() {
-//     context('TBD: parsertests', function () {
+describe('Parser', function() {
+    context('Valid CSS (only classes)', function() {
+        it('should get a object without any error', done => {
+            let fixture = 'test/fixtures/sample.css',
+                result  = nativeCSS.convert(fixture)
 
-//     })
-// })
+            // got object?
+            expect(typeof result).to.equal('object')
+            // test class .a
+            assert.deepEqual(result['a'], {
+                'backgroundColor': '#111',
+                'color': '#000'
+            })
+            // test class .b
+            assert.deepEqual(result['b'], {
+                'background': '#222',
+                'color': '#111',
+                'fontSize': '15px'
+            })
+            // test class .c
+            assert.deepEqual(result['c'], {
+                'color': '#000'
+            })
+
+            done()
+        })
+    })
+})
